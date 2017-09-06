6b29e1dda42d38c1416c4a5a99616e67f72859e1
Saúl Rivera <saulriveal@gmail.com>
2017-09-06 11:52:33 -0500

    Tour of Heroes from Angular 4 until Splicing App prepared for routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1>{{title}}</h1>
+    <my-heroes></my-heroes>
+  `
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
 }
